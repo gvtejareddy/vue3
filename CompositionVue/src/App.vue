bda7f0862c8262f86610bc6f4466d95439d762fb
VEERA TEJA REDDY GOPIREDDY <gvtejareddy1@gmail.com>
2024-02-25 20:15:43 +0100

    push

--- CompositionVue/src/App.vue
+++ CompositionVue/src/App.vue
@@ -1,47 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+let count = ref(0);
+
+const addCount = () => {
+  console.log(count.value);
+  count.value = count.value + 1;
+}
+
+const subCount = () => {
+  console.log(count.value);
+  if (count.value > 0)
+    count.value = count.value - 1;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>Check Vue 3 For Composition</div>
+  <div> {{ count }}</div>
+  <button @click=addCount>Plus</button>
+  <button @click=subCount>Sub</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
