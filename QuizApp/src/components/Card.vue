40b74c702ece1ec5a9e79b75548240539ccdd46d
VEERA TEJA REDDY GOPIREDDY <gvtejareddy1@gmail.com>
2024-02-28 20:05:30 +0100

    PUSH

---
+++ QuizApp/src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div class="card">
+        <img  alt="">
+        <div class="card-text">
+            <h2></h2>
+            <p> questions</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0
+  }
+
+  .card .card-text {
+    padding: 0 5px
+  }
+
+  .card .card-text h2 {
+    font-weight: bold;
+  }
+</style>
