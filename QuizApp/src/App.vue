7558952b74b4f9c7682a12e5b6be6c34e348d5d5
VEERA TEJA REDDY GOPIREDDY <gvtejareddy1@gmail.com>
2024-07-03 22:00:37 +0200

    quiz-app

--- QuizApp/src/App.vue
+++ QuizApp/src/App.vue
@@ -1,16 +1,79 @@
 <script setup>
-import {RouterView} from "vue-router"
+import q from "./data/quizes.json";
+import { ref } from "vue";
+const quizes= ref(q);
+
 </script>
 
 <template>
- <div class="container">
-    <RouterView />
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search...">
+    </header>
+    <div v-for="quiz in quizes" class="gallery" :key="quiz.id">
+      <a target="_blank" href="">
+        <img :src="quiz.img" :alt="quiz.name" width="600" height="400">
+      </a>
+      <div class="card-text">
+            <h2>{{ quiz.name }}</h2>
+            <p>{{ quiz.questions.length }} questions</p>
+        </div>
+    </div>
   </div>
 </template>
 
+
 <style scoped>
-  .container {
-    max-width: 1000px;
-    margin: 0 auto
+.container {
+  max-width: 1000px;
+  margin: 0 auto
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+.card-text {
+    padding: 0 5px
+  }
+
+  .card-text h2 {
+    font-weight: bold;
   }
 </style>
